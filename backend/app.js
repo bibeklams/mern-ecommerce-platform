76916cde88461f023e5be3fe44d34e21a9543dd9
bibek Tamang <bibeklama123asd@gmail.com>
2026-07-05 12:24:42 +0545

    routes addeed

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import errorMiddleware from "./middleware/error.middleware.js";
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+
+app.use("/api/auth", authRoutes);
+
+// Error middleware (always last)
+app.use(errorMiddleware);
 
 export default app;
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,26 @@
+import express from "express";
+const router = express.Router();
+
+import * as authController from "../controllers/auth.controller.js";
+import protect from "../middleware/auth.middleware.js";
+
+// Register
+router.post("/register", authController.register);
+
+// Verify email
+router.post("/verify-email", authController.verifyEmail);
+
+// Refresh token (NO protect)
+router.post("/refreshToken", authController.refreshToken);
+
+// Logout (protected)
+router.post("/logout", protect, authController.logout);
+
+// Logout from all devices (protected)
+router.post(
+  "/logoutFromAllDevice",
+  protect,
+  authController.logoutFromAllDevice,
+);
+
+export default router;
